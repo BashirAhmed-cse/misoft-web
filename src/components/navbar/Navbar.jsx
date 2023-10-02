8e03eadfae6e5e59302f2605910a172470326fd7
bashirahmed-cse <bashirahmed.cse@gmail.com>
2023-10-02 15:46:48 +0600

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,13 +9,14 @@ const Navbar = () => {
          <a className='cursor-pointer'>MisoftIt</a>
        </div>
         <div className='nav my-5 flex items-center gap-8'>
-          <a className='hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 '>Home</a>
-          <a>Home</a>
-          <a>Home</a>
-          <a>Home</a>
+          <a className='custom-color'>Home</a>
+          <a>Services</a>
+          <a>Technologies</a>
+          <a>Process</a>
+          <a>Portfolio</a>
         </div>
           <div className='contact my-5'>
-             <a>Contact</a>
+             <button className='rounded-xl outline outline-offset-4 outline-1'><span className='mx-3'>Let's talk</span></button>
           </div>
        </div>
       </div>
